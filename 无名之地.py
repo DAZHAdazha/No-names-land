40a2ca0c8bcb7b900586f749d4d413a96eced9f3
Leyusf <53003567+Leyusf@users.noreply.github.com>
2019-09-07 14:50:16 +0800

    Update 无名之地.py

--- 无名之地.py
+++ 无名之地.py
@@ -4,7 +4,6 @@ import sys
 import json
 import os
 
-
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
@@ -18,7 +17,7 @@ pygame.init()
 screen = pygame.display.set_mode(size)
 fclock = pygame.time.Clock()
 
-font = pygame.font.Font("ShenYunSuXinTi-2.ttf", 40)
+font = pygame.font.Font("ShenYunSuXinTi-2.ttf", 32)
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 character_images = pygame.image.load("角色.png")
@@ -33,21 +32,152 @@ achievement_image = achievement_image.move(width - 180, height - 60)
 pygame.display.set_caption("无名之地")
 
 
+class Prop:
+
+    def __init__(self, name, pos):
+        """type include 1,2,3,4,5,6"""
+        """1 = weapon, 2 = helmet, 3 = armor, 4 = shoes, 5 = ornament, 6 = title"""
+        self.name = name
+        self.level = 1
+        self.exp = 0
+        self.need_exp = 10
+        self.num = 0
+        self.pos = pos
+
+    def set_prop_ability(self, attack, defence, health, magic, critical, speed, luck):
+        self.attack = attack
+        self.defence = defence
+        self.health = health
+        self.magic = magic
+        self.critical = critical
+        self.speed = speed
+        self.luck = luck
+
+    def prop_growth_ability(self, attack, defence, health, magic, critical, speed, luck):
+        self.grow_attack = attack
+        self.grow_defence = defence
+        self.grow_health = health
+        self.grow_magic = magic
+        self.grow_critical = critical
+        self.grow_speed = speed
+        self.grow_luck = luck
+
+    def growth(self):
+        self.attack += self.grow_attack
+        self.defence += self.grow_defence
+        self.health += self.grow_health
+        self.magic += self.grow_magic
+        self.critical += self.grow_critical
+        self.speed += self.grow_speed
+        self.luck += self.grow_luck
+
+    def up_level(self, remainder):
+        self.level += 1
+        self.need_exp *= 1.5
+        self.exp = remainder
+        self.growth()
+
+    def add_prop_list(self, props_list):
+        """add a prop to props_list"""
+        dic = {'name': self.name, 'attack': self.attack, 'defence': self.defence, 'health': self.health, 'magic': self.magic,
+               'critical': self.critical, 'speed': self.speed, 'luck': self.luck, 'level': self.level,
+               'exp': self.exp, 'need_exp':self.need_exp, 'grow_attack': self.grow_attack, 'grow_defence': self.grow_defence,
+               'grow_health': self.grow_health, 'grow_magic': self.grow_magic, 'grow_critical': self.grow_critical,
+               'grow_speed': self.grow_speed, 'grow_luck': self.grow_luck, 'num': self.num, 'pos': self.pos}
+        props_list.append(dic)
+
+    def alter_prop_list(self, props_list):
+        """alter a prop in contents"""
+        for i in props_list:
+            if i['name'] == self.name:
+                i['attack'] = self.attack
+                i['defence'] = self.defence
+                i['health'] = self.health
+                i['magic'] = self.magic
+                i['critical'] = self.critical
+                i['speed'] = self.speed
+                i['luck'] = self.luck
+                i['level'] = self.level
+                i['need_exp'] = self.need_exp
+                i['exp'] = self.exp
+                i['grow_attack'] = self.grow_attack
+                i['grow_defence'] = self.grow_defence
+                i['grow_health'] = self.grow_health
+                i['grow_magic'] = self.grow_magic
+                i['grow_luck'] = self.grow_luck
+                i['grow_speed'] = self.grow_speed
+                i['grow_critical'] = self.grow_critical
+                i['num'] = self.num
+                i['pos'] = self.pos
+
+    def create_prop(self, attack, defence, health, magic, critical, speed, luck, g_attack, g_defence,
+                    g_health, g_magic, g_critical, g_speed, g_luck, props_list):
+        self.set_prop_ability(attack, defence, health, magic, critical, speed, luck)
+        self.prop_growth_ability(g_attack, g_defence, g_health, g_magic, g_critical, g_speed, g_luck)
+        self.add_prop_list(props_list)
+
+    def get_exp(self, get_exp, props_list):
+        self.exp += get_exp
+        while self.exp >= self.need_exp:
+            remainder = self.exp - self.need_exp
+            self.up_level(remainder)
+        self.alter_prop_list(props_list)
+
+    def get_new_prop(self, props_list):
+        for i in props_list:
+            if i['name'] == self.name:
+                i['num'] += 1
+                break
+
+    def destroy_prop(self, props_list):
+        for i in props_list:
+            if i['name'] == self.name:
+                if i['num'] > 0:
+                    i['num'] -= 1
+                    break
+
+    def grow_prop(self, prop, props_list):
+        for i in props_list:
+            if i['name'] == prop.name:
+                i.num -= 1
+            if self.name == i['name']:
+                exp = prop.need_exp * prop.level
+                self.get_exp(exp, props_list)
+
+
+def load_props(contents, props_list):
+    for i in contents['props']:
+        p = Prop(i['name'])
+        p.set_prop_ability(i['attack'], i['defence'], i['health'], i['magic'], i['critical'], i['speed'], i['luck'])
+        p.prop_growth_ability(i['grow_attack'], i['grow_defence'], i['grow_health'], i['grow_magic'], i['grow_critical'],
+                              i['grow_speed'], i['grow_luck'])
+        p.exp = i['exp']
+        p.need_exp = i['need_exp']
+        p.level = i['level']
+        p.num = i['num']
+        p.pos = i['pos']
+        props_list.append(p)
+
+
+def down_props(contents, props_list):
+    contents['props'] = props_list
+
+
 class Drug:
 
     def __init__(self, name):
         self.name = name
 
-    def drug_capacity(self, size=5, num=0):
-        self.size = size
+    def drug_capacity(self, drug_size=5, num=0):
+        self.drug_size = drug_size
         self.num = num
 
     def drug_effect(self, attack, defence, health, speed, magic):
-        self.attack = attack * self.size
-        self.defence = defence * self.size
-        self.health = health * self.size
-        self.speed = speed * self.size
-        self.magic = magic * self.size
+        self.attack = attack * self.drug_size
+        self.defence = defence * self.drug_size
+        self.health = health * self.drug_size
+        self.speed = speed * self.drug_size
+        self.magic = magic * self.drug_size
 
     def change_drug_dic(self, drug_list):
         dic = {'name': self.name, 'attack': self.attack, 'defence': self.defence, 'health': self.health,
@@ -61,8 +191,8 @@ class Drug:
             if i['name'] == self.name:
                 i['num'] = self.num
 
-    def create_drug(self, attack, defence, health, speed, magic, num, size, drug_list):
-        self.drug_capacity(size, num)
+    def create_drug(self, attack, defence, health, speed, magic, num, drug_size, drug_list):
+        self.drug_capacity(drug_size, num)
         self.drug_effect(attack, defence, health, speed, magic)
         self.change_drug_dic(drug_list)
 
@@ -100,6 +230,29 @@ class Character:
         self.level = 1
         self.exp = 0
         self.need_exp = 10
+        self.position = []
+
+    def equip_prop(self, prop):
+        self.attack = self.attack + prop.attack
+        self.defence = self.defence + prop.defence
+        self.health = self.health + prop.health
+        self.magic = self.magic + prop.magic
+        self.critical = self.critical + prop.critical
+        self.speed = self.speed + prop.speed
+        self.luck = self.luck + prop.luck
+        self.insight = self.insight + prop.insight
+        self.position.append(prop.pos)
+
+    def unload_prop(self, prop):
+        self.attack = self.attack - prop.attack
+        self.defence = self.defence - prop.defence
+        self.health = self.health - prop.health
+        self.magic = self.magic - prop.magic
+        self.critical = self.critical - prop.critical
+        self.speed = self.speed - prop.speed
+        self.luck = self.luck - prop.luck
+        self.insight = self.insight - prop.insight
+        self.position.remove(prop.pos)
 
     def set_growth_ability(self, attack, defence, health, magic, critical, speed, luck, insight):
         self.grow_attack = attack
@@ -121,7 +274,7 @@ class Character:
         self.luck = luck
         self.insight = insight
 
-    def growth (self):
+    def growth(self):
         self.attack += self.grow_attack
         self.defence += self.grow_defence
         self.health += self.grow_health
@@ -173,12 +326,12 @@ class Character:
                'critical': self.critical, 'speed': self.speed, 'luck': self.luck, 'insight': self.insight, 'level': self.level,
                'exp': self.exp, 'need_exp':self.need_exp, 'grow_attack': self.grow_attack, 'grow_defence': self.grow_defence,
                'grow_health': self.grow_health, 'grow_magic': self.grow_magic, 'grow_critical': self.grow_critical,
-               'grow_speed': self.grow_speed, 'grow_luck': self.grow_luck, 'grow_insight': self.grow_insight}
+               'grow_speed': self.grow_speed, 'grow_luck': self.grow_luck, 'grow_insight': self.grow_insight, 'position': self.position}
         characters_list.append(dic)
 
     def alter_character_ability(self, contents):
         """alter a character in contents"""
-        for i in contents:
+        for i in contents['characters']:
             if i['name'] == self.name:
                 i['attack'] = self.attack
                 i['defence'] = self.defence
@@ -199,6 +352,7 @@ class Character:
                 i['grow_speed'] = self.grow_speed
                 i['grow_insight'] = self.grow_insight
                 i['grow_critical'] = self.grow_critical
+                i['position'] = self.position
 
     def create_character(self, attack, defence, health, magic, critical, speed, luck, insight, g_attack, g_defence,
                     g_health, g_magic, g_critical, g_speed, g_luck, g_insight, dic):
@@ -222,6 +376,7 @@ def load_characters(contents, characters_list):
         ch.exp = i['exp']
         ch.need_exp = i['need_exp']
         ch.level = i['level']
+        ch.position = i['position']
         characters_list.append(ch)
 
 
@@ -267,6 +422,21 @@ def show_words(words, coord):
     text = texts.get_rect()
     text.center = (coord[0], coord[1])
     screen.blit(texts, text)
+
+
+def show_attr(character, coord):
+    """change 'attack...' to '攻击' """
+    show_words('经验:' + str(character.exp) + '/' + str(character.need_exp), (coord[0] + 72, coord[1]))
+    show_words('攻击:' + str(character.attack), (coord[0], coord[1] + 50))
+    show_words('防御:' + str(character.defence), (coord[0] + 145, coord[1] + 50))
+    show_words('生命:' + str(character.health), (coord[0], coord[1] + 100))
+    show_words('魔法:' + str(character.magic), (coord[0] + 145, coord[1] + 100))
+    show_words('暴击:' + str(character.critical), (coord[0], coord[1] + 150))
+    show_words('速度:' + str(character.speed), (coord[0] + 145, coord[1] + 150))
+    show_words('幸运:' + str(character.luck), (coord[0], coord[1] + 200))
+    show_words('洞视:' + str(character.insight), (coord[0] + 145, coord[1] + 200))
+    show_words('等级:' + str(character.level), (coord[0], coord[1] + 250))
+    """one more attr"""
 
 
 def is_new(contents):
@@ -346,7 +516,8 @@ while(True):
         pygame.draw.line(screen, GREY, (100, height / 2), (width - 100, height / 2), 5)
         show_words(character_list[0].name, ((width - 200) / 6 + 100, 150))
         show_words(character_list[1].name, ((width - 200) / 2 + 100, 150))
-        show_words('攻击', ((width - 200) / 6 * 5 + 100, 170))
+        show_attr(character_list[0], ((width - 200) / 6 + 20, height / 2 + 30))
+        show_attr(character_list[1], ((width - 200) / 2 + 20, height / 2 + 30))
         draw_window()
         while(True):
             if close_window() == 1:
@@ -354,7 +525,7 @@ while(True):
     if (width - 120 < mouse_pos[0] < width - 60 and height - 60 < mouse_pos[1] < height and mouse_pressed[0] == 1):
         """bag"""
         draw_window()
-        while (True):
+        while ():
             if close_window() == 1:
                 break
     if (width - 180 < mouse_pos[0] < width - 120 and height - 60 < mouse_pos[1] < height and mouse_pressed[0] == 1):
